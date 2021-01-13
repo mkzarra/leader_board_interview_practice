aceeab5a94ec715d65503515988b0bf823061dfc
Mike Zarra <mkzarra@Mikes-Air.fios-router.home>
2021-01-13 13:59:18 -0500

    solution with working expiration

--- leaderboard.js
+++ leaderboard.js
@@ -89,29 +89,86 @@ We have provided stubbed out code and tests for you below. Please note that thes
 
 
 // Your code goes here. Feel free to make helper classes if needed
-class Player {
-	constructor() {
-		this.scores = [];
-		this.average = this.getAverage()
-	}
 
-	getAverage = () => {
-		return this.scores.reduce((accumulator, current) => accumulator + current, 0) / this.scores.length;
-	}
+class Player {
+  constructor() {
+    this.scores = [];
+    this.avg = this.getAvg();
+  }
+
+  getAvg = () => {
+    return this.scores.reduce((a, c) => a + c.score, 0) / this.scores.length || 0;
+  }
+
+  addNewScore = (score, dateCreated) => {
+    this.scores.push({ score, dateCreated });
+  }
+
+	expireScore = (exp) => {
+		for (let i = 0; i < this.scores.length; i++) {
+			console.log(this.scores[i].dateCreated, exp)
+			if (this.scores[i].dateCreated >= exp) {
+				this.scores[i] = 0;
+      }
+		}
+		this.scores = this.scores.filter(Boolean);
+    this.avg = this.getAvg();
+  }
+
+  resetScores = () => {
+    return this.scores = [];
+  }
 }
 
 class LeaderBoard {
-	constructor() {
-		this.players = {}
+  constructor() {
+    this.players = {};
   }
 
-  add_score = (player_id, score) => {
+  add_score = (player_id, score, dateCreated) => {
+    const player = this.players[player_id] || new Player();
+    this.players[player_id] = player;
+    player.addNewScore(score, dateCreated);
+		player.avg = player.getAvg();
+		
+    return player.avg;
+  }
+
+  rankPlayers = (num_players, top = true) => {
+    const playersRanked = Object.entries(this.players);
+    
+    if (!top) {
+      playersRanked.sort((a, b) => a[1].avg >= b[1].avg ? 1 : -1);
+    } else {
+      playersRanked.sort((a, b) => a[1].avg >= b[1].avg ? -1 : 1);
+    }
+
+    return playersRanked.slice(0, num_players).map(p => parseInt(p[0]));
   }
 
   top = (num_players) => {
+    return this.rankPlayers(num_players);
+  }
+
+  bottom = (num_players) => {
+    return this.rankPlayers(num_players, false);
+  }
+
+	setExpiration = (exp) => {
+		let returnValue = []
+    for (let player in this.players) {
+			this.players[player].expireScore(exp);
+			returnValue.push({ [player]: this.players[player].scores.map(obj => JSON.stringify(obj)) })
+		}
+		
+    return returnValue;
   }
 
   reset = (player_id) => {
+    const player = this.players[player_id] || new Player();
+    this.players[player_id] = player;
+    player.scores = player.resetScores();
+    player.avg = player.getAvg();
   }
 }
 
@@ -123,24 +180,29 @@ function array_equals(a, b) {
   if (a.length != b.length) return false;
   for (var i = 0; i < a.length; ++i) {
     if (a[i] !== b[i]) return false;
-  }
+	}
+	
   return true;
 }
 
 var leader_board = new LeaderBoard();
 
-leader_board.add_score(1, 50);
-console.log(leader_board.add_score(2, 80) == 80);
-console.log(leader_board.add_score(2, 70) == 75);
-console.log(leader_board.add_score(2, 60) == 70);
+leader_board.add_score(1, 50, 7);
+console.log(leader_board.add_score(2, 80, 8) == 80);
+console.log(leader_board.add_score(2, 7, 12) == 75);
+console.log(leader_board.add_score(2, 60, 7) == 70);
 console.log('Add score should return the average. test with 1 score');
-console.log(leader_board.add_score(3, 90) == 90);
+console.log(leader_board.add_score(3, 90, 6) == 90);
 console.log('Add score should return the average. test with 2 scores');
-console.log(leader_board.add_score(3, 85) == 87.5);
+console.log(leader_board.add_score(3, 85, 4) == 87.5);
 console.log('Top 3 [' + leader_board.top(3) + '] should equal [3, 2, 1]:');
 console.log(array_equals(leader_board.top(3), [3, 2, 1]));
 console.log('Top 2 [' + leader_board.top(2) + '] should equal [3, 2]:');
 console.log(array_equals(leader_board.top(2), [3, 2]));
-leader_board.reset(3);
+// leader_board.reset(3);
 console.log('After reset top 3 [' + leader_board.top(3) + '] should equal [2, 1, 3]');
 console.log(array_equals(leader_board.top(3), [2, 1, 3]));
+console.log('After reset bottom 3 [' + leader_board.bottom(3) + '] should equal [3, 1, 2]');
+console.log('Bottom 2 [' + leader_board.bottom(2) + '] should equal [3, 1]');
+console.log(leader_board.players[1]);
+console.log('unexpired scores: ', leader_board.setExpiration(5000),'\nexpired scores: ', leader_board.setExpiration(6));
